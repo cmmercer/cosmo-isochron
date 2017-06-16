1ae5b43b10ce8bcc7e697ff9272f06a0c6a529d9
Cameron Mark Mercer <cameron.m.mercer@asu.edu>
2017-06-15 17:00:42 -0700

    Minor documentation edits and other enhancements to workflow flexibility.

--- isochron.py
+++ isochron.py
@@ -36,7 +36,7 @@ def loadDataset(path,delim='\t',idx_col=0):
 
   Arguments:
   - path    - The path to the datafile.
-  - delim   - The delimeter to use when reading the file; default = \t.
+  - delim   - The delimeter to use when reading the file; default = tab.
   - idx_col - The column to make the DataFrame index; default = 0.
 
   Returns: the dataset, as a Pandas DataFrame.
@@ -51,7 +51,7 @@ def extractData(ds,indexes=[0,1,2,3,4],selcol=None,selval=None,exclude=None):
 
   Arguments:
   - ds      - The DataFrame from which to extract a subset of data.
-  - indexes - The default selection indexes, in the order: [x,sx,y,sy,rho] => [0,1,2,3,4].
+  - indexes - The default selection indexes, in the order: [x,sx,y,sy,rho]; default => [0,1,2,3,4].
   - selcol  - The name of a column that will be used to select data records.
   - selval  - A list of values that qualify a data record to be selected.
   - exclude - A list of items to exclude; these items must be values from the index
@@ -67,7 +67,7 @@ def extractData(ds,indexes=[0,1,2,3,4],selcol=None,selval=None,exclude=None):
   if selcol is None:
     idx = range(len(ds))
   else:
-    sci = list(ds.columns).index('Material')
+    sci = list(ds.columns).index(selcol)
     if not isinstance(selval,list):
       selval = [selval]
     for i in range(len(ds)):
@@ -207,7 +207,7 @@ def plotEnvelope(ax,a,sa,b,sb,zid,level=2,color='#999999'):
   psize = 2**12
   ra = r.normal(a,sa,(psize))
   rb = r.normal(b,sb,(psize))
-  # Generate 2-sigma values along mesh.
+  # Generate n-sigma values along mesh (based on confidence level)
   mx = np.linspace(0,xl[1],msize)
   m2s = np.zeros(msize)
   for i in range(msize):
@@ -362,7 +362,8 @@ def weighted_oli_2d(data,york_res,cutoff=4.0):
   Applies the Hampel outlier identifier to the weighted residuals (chi**2 values)
   of the data from the specified York regression results.
   '''
-  idx = hampel(chi2_2d(data,york_res),cutoff)
+  (capW, capS) = chi2_2d(data,york_res)
+  idx = hampel(capS,cutoff)
   oli = []
   for i in idx:
     oli.append(data[5][i])
@@ -382,9 +383,11 @@ def chi2_2d(data,york_res):
   # Get weights for data, and model weights with error correlations.
   wX, wY = 1.0/sx**2, 1.0/sy**2
   capW = np.zeros(n)
+  capS = np.zeros(n)
   for i in range(n):
     capW[i] = (wX[i]*wY[i])/(wX[i] + (b**2)*wY[i] - 2.0*b*rho[i]*np.sqrt(wX[i]*wY[i]))
-  return capW
+    capS[i] = capW[i]*(y[i] - b*x[i] - a)**2
+  return capW, capS
 
 # Hampel.
 def hampel(values,cutoff=4.0):
@@ -394,7 +397,8 @@ def hampel(values,cutoff=4.0):
   Returns: a list of indexes of potential outliers, or an empty list if none were identified.
   '''
   # Calculate absolute deviations from the median, and scaled MADM value.
-  viMed = abs(values - np.median(values))
+  med = np.median(values)
+  viMed = abs(values - med)
   s = 1.4826*np.median(viMed)
   # Identify potential outliers.
   oli = viMed/s
@@ -403,3 +407,53 @@ def hampel(values,cutoff=4.0):
     if oli[i] > cutoff:
       idx.append(i)
   return idx
+
+# --------------------------------------------------------------------------------
+# Quick bootstrap function for 61015 and 60315 cosmochron search.
+
+def bootstrap_cosmochron(path,rej=[],level=2):
+  # This method assumes the data are sorted in descending order of uncertainty in 37/36.
+  solar = 0.187
+  ds = loadDataset(path)
+  pd = extractData(ds,exclude=rej)
+  res = yorkRegression(pd)
+  count = 0
+  limit = len(ds) - 1
+  while abs(res[0] - solar) > level*res[1]:
+    rej.append(pd[5][0])
+    pd = extractData(ds,exclude=rej)
+    res = yorkRegression(pd)
+    count += 1
+    if count == limit:
+      break
+  (ax,zid,fig) = initPlot()
+  p.ylim([0,2]); p.xlim([0,5000])
+  zid = plotRegression(ax,res[0],res[2],zid)
+  zid = plotEnvelope(ax,res[0],res[1],res[2],res[3],zid,level=1)
+  zid = plotEnvelope(ax,res[0],res[1],res[2],res[3],zid)
+  zid = plotData(ax,pd,zid)
+  print_stats(pd,res)
+
+def bootstrap_isochron(path,rej=[],level=2):
+  # This method assumes the data are sorted in descending order of uncertainty in 39/36.
+  solar = 1
+  ds = loadDataset(path)
+  pd = extractData(ds,exclude=rej)
+  res = yorkRegression(pd)
+  count = 0
+  limit = len(ds) - 1
+  while abs(res[0] - solar) > level*res[1]:
+    rej.append(pd[5][0])
+    pd = extractData(ds,exclude=rej)
+    res = yorkRegression(pd)
+    count += 1
+    if count == limit:
+      break
+  (ax,zid,fig) = initPlot()
+  p.ylim([0,400000]); p.xlim([0,1640])
+  zid = plotRegression(ax,res[0],res[2],zid)
+  zid = plotEnvelope(ax,res[0],res[1],res[2],res[3],zid)
+  zid = plotData(ax,pd,zid)
+  print_stats(pd,res)
+  for idi in pd[5]:
+    print idi
