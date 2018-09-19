4a5ea862c500011aae8975e911f5fb115c2f48bc
Cameron Mark Mercer <cameron.m.mercer@asu.edu>
2018-09-19 16:48:21 -0700

    Added license headers to isochron and rhomc files.

--- rhomc.py
+++ rhomc.py
@@ -1,6 +1,24 @@
-# rhomc.py
-# A quick library of scripts used to determine error correlation coefficients
+# rhomc.py - A quick library of scripts used to determine error correlation coefficients
 # using analytical and Monte Carlo approaches.
+#
+# Copyright (C) 2018, Cameron M. Mercer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# A quick library of scripts used to determine error correlation coefficients
+# using analytical and Monte Carlo approaches. This is designed to be used with
+# the isochron.py function library.
 #
 
 import matplotlib as mpl       #Import matplotlib alone to reset the graphics renderer.
